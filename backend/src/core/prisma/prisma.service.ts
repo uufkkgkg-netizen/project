98eb450460041401fd9df30cbc26b5325943c948
uufkkgkg-netizen <uufkkgkg@gmail.com>
2026-06-12 23:16:29 +0300

    fix: update prisma service constructor

--- backend/src/core/prisma/prisma.service.ts
+++ backend/src/core/prisma/prisma.service.ts
@@ -16,7 +16,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     const connectionString = process.env.DATABASE_URL;
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
-    super({ adapter });
+   super();
   }
 
   async onModuleInit() {
